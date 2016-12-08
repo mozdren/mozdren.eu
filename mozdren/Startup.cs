c3913c9d21aa657b09cba0a7a406c9225b55a45f
Karel Mozdren <mozdren@gmail.com>
2016-12-08 15:55:36 +0100

    Basic empty project

---
+++ mozdren/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mozdren.Startup))]
+namespace mozdren
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
